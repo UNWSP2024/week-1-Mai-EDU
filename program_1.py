4db53310f61c5cb9a1b5eec52ea3cd9a530efdc1
Mai-EDU <malillie@students.unwsp.edu>
2024-09-06 12:28:44 -0500

    Add files via upload

--- program_1.py
+++ program_1.py
@@ -1,6 +1,4 @@
-def hello_world():
-    # Update the message below to say "Hello World", 
-    # Python cares about white space, so watch your indentation.
-    print('Hello ...')
-
-hello_world()
+#Programmer: Mai
+#Date: Sep. 6, 2024
+#Title: Hello World Program
+print('Hello, World.')
